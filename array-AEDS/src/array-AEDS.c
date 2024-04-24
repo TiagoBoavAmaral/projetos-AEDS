a9150a9c855f0cb3fe6af9fa7c53033f8a92f4c7
Tiago Boaventura Amaral <161142053+TiagoBoavAmaral@users.noreply.github.com>
2024-04-24 11:11:33 -0300

    Update array-AEDS.c

--- array-AEDS/src/array-AEDS.c
+++ array-AEDS/src/array-AEDS.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,12 +5,12 @@ int main(void) {
 	//exemplo de declaracao e inicializacao de um array de numeros inteiros
 	int numeros[5] = { 1, 2, 3, 4, 5 };
 	//exemplo de declaracao e inicializacao de um array de strings
-	char clubes[] [30] = {"Atletico Mineiro", "Athletico Paranaense", "Atletico de Madrid" };
+	char clubes[] [30] = {"Atletico Mineiro, ", "Athletico Paranaense, ", "Atletico de Madrid." };
 
 	//loop for para percorrer o array e imprimir seus elementos
 	printf("Elementos do array de inteiros: ");
 	for (int i = 0; i < 5; i++) {
-		printf("%d", numeros[i]);
+		printf("%d ", numeros[i]);
 	}
 
 	//loop for para percorrer o array de strings e imprimir os nomes dos clubes
